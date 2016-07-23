603d4d6070283fe347f86e192e79b2942e2962e1
kevinl <kevinliu987@gmail.com>
2016-07-23 12:31:18 -0700

    updated chatbox to only display messages with created at timestamp after a client's login time

--- public/bundle.js
+++ public/bundle.js
@@ -23743,7 +23743,8 @@
 	      status: 'Disconnected',
 	      singer: {},
 	      singersInRoom: [],
-	      messageList: []
+	      messageList: [],
+	      startConnection: ''
 	    };
 	  },
 
@@ -23767,7 +23768,8 @@
 	      this.emit('enter', member);
 	    }
 
-	    this.setState({ status: "Connected" });
+	    this.setState({ status: "Connected",
+	      startConnection: new Date() });
 	  },
 
 	  //all outgoing data to server will flow through this emit function
@@ -23806,7 +23808,12 @@
 	  },
 
 	  updateMessages: function updateMessages(newMessages) {
-	    this.setState({ messageList: newMessages });
+	    var that = this;
+	    var messageListDisplay = newMessages.filter(function (msg) {
+	      return new Date(msg.createdAt) >= that.state.startConnection;
+	    });
+
+	    this.setState({ messageList: messageListDisplay });
 	  },
 
 	  render: function render() {
@@ -31439,7 +31446,7 @@
 
 		sendMessage: function sendMessage() {
 			var message = React.findDOMNode(this.refs.inputMessage).value;
-			this.props.emitMessage('sendMessage', { name: this.props.singer.name, message: message });
+			this.props.emitMessage('sendMessage', { name: this.props.singer.name, message: message, startConnection: this.props.startConnection });
 
 			// Clears input field on submit
 			React.findDOMNode(this.refs.inputMessage).value = '';
